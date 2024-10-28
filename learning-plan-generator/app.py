72999f6940651636c0f153887855852b1e8a8769
Nathan Robertson <46699097+n-c-robertson@users.noreply.github.com>
2024-10-28 13:28:47 -0600

    Update app.py

--- learning-plan-generator/app.py
+++ learning-plan-generator/app.py
@@ -313,6 +313,7 @@ def generateLearningPlan(message, jobProfile, uploadedFile):
             "label": step.label,
             "duration": step.duration if hasattr(step, 'duration') else None,
             "catalog_url": step.catalog_url if hasattr(step, 'catalog_url') else None,
+            "image_url": step.image_url if hasattr(step, 'image_url') else None,
             "short_description": step.short_description if hasattr(step, 'short_description') else None,
             "long_description": step.long_description if hasattr(step, 'long_description') else None,
             "skills": step.skills if hasattr(step, 'skills') else None,
@@ -358,6 +359,24 @@ def generateLearningPlan(message, jobProfile, uploadedFile):
     }
 
     return response_dict, filtered_titles
+
+
+def horizontalCard():
+    return f"""
+    <div class="card mb-3" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="..." class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>"""
 
 def learning_plan_generator():
     st.title("Learning Plan Generator")
@@ -462,6 +481,9 @@ def learning_plan_generator():
                         for req in step['completion_requirements']:
                             st.write(f"- {req['description']}")
 
+
+            st.markdown(horizontalCard, unsafe_allow_html=True)
+            
             # Divider before Completion Requirements
             st.divider()
 
