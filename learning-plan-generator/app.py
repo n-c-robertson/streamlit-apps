764d858f902d103982cc377144b0969b04add981
Nathan Robertson <46699097+n-c-robertson@users.noreply.github.com>
2024-10-28 16:09:56 -0600

    Update app.py

--- learning-plan-generator/app.py
+++ learning-plan-generator/app.py
@@ -11,7 +11,7 @@ st.markdown("""
         width: 100%;
         height: 100%;
         object-fit: cover;
-        border-top-left-radius: 8px;
+        border-top-left-radius: 4px;
         border-bottom-left-radius: 0px;
     }
     .card-body {
