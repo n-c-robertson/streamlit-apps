0db052ebfc03b8af54830558a8dca8ba7a75ce9a
Nathan Robertson <46699097+n-c-robertson@users.noreply.github.com>
2024-10-28 13:50:22 -0600

    Update app.py

--- learning-plan-generator/app.py
+++ learning-plan-generator/app.py
@@ -4,7 +4,8 @@ import streamlit.components.v1 as components
 
 # Streamlit hack for getting bootstrap styling.
 st.markdown("""
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 <style>
         .card-img-left {
             width: 100%;
