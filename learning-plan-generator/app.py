f056eb7f49a46ac524d5b2de8f9b185487fd66d1
Nathan Robertson <46699097+n-c-robertson@users.noreply.github.com>
2024-10-28 13:33:11 -0600

    Update app.py

--- learning-plan-generator/app.py
+++ learning-plan-generator/app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 
 # Streamlit hack for getting bootstrap styling.
 st.markdown("""
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 """, unsafe_allow_html=True)
 
 # File processing.
