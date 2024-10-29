1d9c120d091efcb97d351cfcf74683dd71832fe2
Nathan Robertson <46699097+n-c-robertson@users.noreply.github.com>
2024-10-28 21:52:47 -0600

    Update app.py

--- learning-plan-generator/app.py
+++ learning-plan-generator/app.py
@@ -3,12 +3,10 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 page_bg_img = '''
-<style>
 body {
 background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
 background-size: cover;
 }
-</style>
 '''
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
