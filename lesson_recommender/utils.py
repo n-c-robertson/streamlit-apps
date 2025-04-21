4c0e4301019d794e02b787df6f8b0db59980e6ef
Nathan Robertson <46699097+n-c-robertson@users.noreply.github.com>
2025-04-21 10:24:19 -0600

    Update utils.py

--- lesson_recommender/utils.py
+++ lesson_recommender/utils.py
@@ -11,7 +11,7 @@ def fetch_data():
 	#url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 	#df = pd.read_csv(url, dtype=str, sep=',',header=0)
 	#df = pd.read_csv('lesson_catalog.csv')
-	df = pd.read_csv(url='https://raw.githubusercontent.com/n-c-robertson/streamlit-apps/refs/heads/main/lesson_recommender/lesson_catalog.csv')
+	df = pd.read_csv('https://raw.githubusercontent.com/n-c-robertson/streamlit-apps/refs/heads/main/lesson_recommender/lesson_catalog.csv')
 	return df
 
 def display_results(df_search):
